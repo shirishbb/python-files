08ef0d45338123fa87db9ba51e927d18667609f7
shirishbb <shirish.bagewadi@gmail.com>
2021-06-30 23:03:16 +0530

    addfile

---
+++ chupacabra.py
@@ -0,0 +1,18 @@
+while True:
+    str=input("Enter the word: ")
+    if( str == "chupacabra"):
+        print("You've successfully left the loop.")
+        break 
+    else:
+          continue
+OR
+
+
+key="chupacabra"
+while True:
+     word=input("Enter the word: ")
+     if(word == key):
+          print("You've successfully left the loop.")
+          break 
+     else:
+          continue
